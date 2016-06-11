716e99ef60f4a91a4b1766217d53da070ca300aa
philly <sonagi.philly@gmail.com>
2016-06-11 22:47:35 +0900

    publish
    publish

---
+++ ChatBot/Parser.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 사용자: philly
+ * 날짜: 2016-06-11
+ * 시간: 오전 1:25
+ */
+using System;
+using System.Collections.Generic;
+
+namespace ChatBot
+{
+	public static class Parser
+	{
+		private static List<string> userLess = new List<string>()
+		{
+			"a", "an", "the",
+			"am", "is", "are", "was", "were"
+		};
+		public static string[] ParseKorean(string input)
+		{
+			List<string> result = new List<string>();
+			
+			foreach(string s in input.Split(null))
+			{
+				string low = s.ToLower();
+				if(!userLess.Contains(low))
+					result.Add(low);
+			}
+			
+			return result.ToArray();
+		}
+	}
+}
---
+++ ChatBot/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * 사용자: philly
+ * 날짜: 2016-06-11
+ * 시간: 오전 1:08
+ */
+using System;
+
+namespace ChatBot
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			CBR t = new CBR();
+			//t.LoadTable("save.dat");
+			
+			while(true)
+			{
+				Console.ForegroundColor = ConsoleColor.Gray;
+				Console.Write("You : ");
+				string read = Console.ReadLine();
+				Console.WriteLine();
+				
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("Bot : " + t.Eval(read));
+				Console.WriteLine();
+			}
+		}
+	}
+}
