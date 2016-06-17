c1953bcf64b280124e8ac1a048c984ca95a8bc61
philly <sonagi.philly@gmail.com>
2016-06-17 22:38:28 +0900

    Updated Korean parsing without 'Josa'.

--- ChatBot/Parser.cs
+++ ChatBot/Parser.cs
@@ -9,24 +9,29 @@ using kr.ac.kaist.swrc.jhannanum.comm;
 using kr.ac.kaist.swrc.jhannanum.hannanum;
 namespace ChatBot
 {
-	public static class Parser
-	{
-		static Workflow workflow;
-		public static string[] ParseKorean(string input)
-		{
-			if(input == null) return new string[]{};
-			if(String.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input)) return new string[]{};
-			if(workflow  == null)
-			{
-				workflow = WorkflowFactory.getPredefinedWorkflow(WorkflowFactory.WORKFLOW_POS_SIMPLE_09);
-				workflow.activateWorkflow(true);
-			}
-			
-			workflow.analyze(input);
-			LinkedList<Sentence> result = workflow.getResultOfDocument(new Sentence(0, 0, false));
-			
-			List<string> res = new List<string>();
-			foreach (Sentence s in result)
+    public static class Parser
+    {
+        private static List<string> Josa = new List<string>()
+        {
+            "은", "는", "를", "이", "가", "나", "어", "지", "고", "거", "아", "내", "있", "요", "네", "하", "로", "을지", "답니다", "도", "에", "ㅂ니다", "는데요", "었는데" ,"의" ,"이르", "어는", "게" ,"어다" ,"니" ,"에게"
+        };
+
+        static Workflow workflow;
+        public static string[] ParseKorean(string input)
+        {
+            if (input == null) return new string[] { };
+            if (String.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input)) return new string[] { };
+            if (workflow == null)
+            {
+                workflow = WorkflowFactory.getPredefinedWorkflow(WorkflowFactory.WORKFLOW_POS_SIMPLE_09);
+                workflow.activateWorkflow(true);
+            }
+
+            workflow.analyze(input);
+            LinkedList<Sentence> result = workflow.getResultOfDocument(new Sentence(0, 0, false));
+
+            List<string> res = new List<string>();
+            foreach (Sentence s in result)
             {
                 Eojeol[] eojeolArray = s.Eojeols;
                 for (int i = 0; i < eojeolArray.Length; i++)
@@ -36,21 +41,22 @@ namespace ChatBot
                         String[] morphemes = eojeolArray[i].Morphemes;
                         for (int j = 0; j < morphemes.Length; j++)
                         {
-                        	res.Add(morphemes[j]);
+                            if (Josa.Contains(morphemes[j])) continue;
+                            res.Add(morphemes[j]);
                         }
                     }
                 }
             }
-			
-			return res.ToArray();
-		}
-		
-		public static string ParseKakaotalkLog(string line)
-		{
-			if(line == null) return "";
-			string[] spl = line.Split(':');
-			if(spl.Length < 3) return "";
-			return line.Substring(spl[0].Length + spl[1].Length + 3);
-		}
-	}
+
+            return res.ToArray();
+        }
+
+        public static string ParseKakaotalkLog(string line)
+        {
+            if (line == null) return "";
+            string[] spl = line.Split(':');
+            if (spl.Length < 3) return "";
+            return line.Substring(spl[0].Length + spl[1].Length + 3);
+        }
+    }
 }
--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -3,6 +3,9 @@
  * 날짜: 2016-06-11
  * 시간: 오전 1:08
  */
+
+ #define PARSETEST
+
 using System;
 
 namespace ChatBot
@@ -11,22 +14,30 @@ namespace ChatBot
 	{
 		public static void Main(string[] args)
 		{
-			CBR t = new CBR();
-			t.LoadKakaoTalkLog();
-			//t.LoadTable("save.dat");
-			System.Windows.Forms.MessageBox.Show("학습이 완료되었습니다!");
-		
-			while(true)
-			{
-				Console.ForegroundColor = ConsoleColor.Gray;
-				Console.Write("You : ");
-				string read = Console.ReadLine();
-				Console.WriteLine();
-				
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine("Bot : " + t.Eval(read));
-				Console.WriteLine();
-			}
-		}
-	}
+#if PARSETEST
+            while (true) {
+                string[] str = Parser.ParseKorean(Console.ReadLine());
+                foreach (string s in str)
+                    Console.Write(s + ", ");
+                Console.WriteLine(); }
+# else
+            CBR t = new CBR();
+            t.LoadKakaoTalkLog();
+            //t.LoadTable("save.dat");
+            System.Windows.Forms.MessageBox.Show("학습이 완료되었습니다!");
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("You : ");
+                string read = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Bot : " + t.Eval(read));
+                Console.WriteLine();
+            }
+#endif
+        }
+    }
 }
